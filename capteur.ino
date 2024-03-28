d2acccbdb8c822c563f0d7cc82a696c0f02d1480
FwaGras <124463406+FwaGras@users.noreply.github.com>
2024-03-28 17:53:21 +0100

    Add files via upload

---
+++ capteur.ino
@@ -0,0 +1,53 @@
+void setup() {
+  //Setup Channel A
+  pinMode(12, OUTPUT); //Initiates Motor Channel A pin
+  pinMode(9, OUTPUT); //Initiates Brake Channel A pin
+  pinMode(0, INPUT); //Initiates Sesnor Channel A pin
+  //Setup Channel B
+  pinMode(13, OUTPUT); //Initiates Motor Channel B pin
+  pinMode(8, OUTPUT);  //Initiates Brake Channel B pin
+  pinMode(1, INPUT); //Initiates Sensor Channel B pin
+}
+
+void loop(){
+  sensorState1 = digitalRead(1); //Initiates sensor state
+  sensorState2 = digitalRead(2); //The same for the other sensor
+  
+  //Motor A forward @ full speed
+  digitalWrite(12, LOW); //Establishes forward direction of Channel A
+  digitalWrite(9, LOW);   //Disengage the Brake for Channel A
+  analogWrite(3, 123);   //Spins the motor on Channel A at full speed
+
+  //Motor B backward @ half speed
+  digitalWrite(13, LOW);  //Establishes backward direction of Channel B
+  digitalWrite(8, LOW);   //Disengage the Brake for Channel B
+  analogWrite(11, 123);    //Spins the motor on Channel B at half speed
+
+  
+  delay(3000);
+
+  
+  digitalWrite(9, HIGH);  //Engage the Brake for Channel A
+  digitalWrite(8, HIGH);  //Engage the Brake for Channel B
+  
+  delay(1000);
+  
+  if (sensorState1 == HIGH || sensorState2 == HIGH) {
+  // Si les "moustaches" ont une collision => les moteurs font reculer le robot pendant 3 sec 
+  digitalWrite(12, HIGH); //Establishes forward direction of Channel A
+  digitalWrite(9, LOW);   //Disengage the Brake for Channel A
+  analogWrite(3, 123);   //Spins the motor on Channel A at full speed
+  
+  digitalWrite(13, HIGH);  //Establishes backward direction of Channel B
+  digitalWrite(8, LOW);   //Disengage the Brake for Channel B
+  analogWrite(11, 123);    //Spins the motor on Channel B at half speed
+  
+  delay(3000);
+  
+  digitalWrite(9, HIGH);  //Engage the Brake for Channel A
+  digitalWrite(8, HIGH);  //Engage the Brake for Channel B
+  
+  delay(1000);
+  }
+   
+}
