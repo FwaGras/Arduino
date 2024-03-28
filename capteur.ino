d72af8df7fdcc318a094f55599b55ffa956b7d47
FwaGras <124463406+FwaGras@users.noreply.github.com>
2024-03-28 18:12:00 +0100

    Add files via upload

--- capteur.ino
+++ capteur.ino
@@ -2,16 +2,18 @@ void setup() {
   //Setup Channel A
   pinMode(12, OUTPUT); //Initiates Motor Channel A pin
   pinMode(9, OUTPUT); //Initiates Brake Channel A pin
-  pinMode(0, INPUT); //Initiates Sesnor Channel A pin
+  pinMode(6, INPUT); //Initiates Sesnor Channel A pin
   //Setup Channel B
   pinMode(13, OUTPUT); //Initiates Motor Channel B pin
   pinMode(8, OUTPUT);  //Initiates Brake Channel B pin
-  pinMode(1, INPUT); //Initiates Sensor Channel B pin
+  pinMode(7, INPUT); //Initiates Sensor Channel B pin
 }
 
 void loop(){
-  sensorState1 = digitalRead(1); //Initiates sensor state
-  sensorState2 = digitalRead(2); //The same for the other sensor
+  boolean sensorState1;
+  boolean sensorState2;
+  sensorState1 = digitalRead(6); //Initiates sensor state
+  sensorState2 = digitalRead(7); //The same for the other sensor
   
   //Motor A forward @ full speed
   digitalWrite(12, LOW); //Establishes forward direction of Channel A
@@ -22,15 +24,6 @@ void loop(){
   digitalWrite(13, LOW);  //Establishes backward direction of Channel B
   digitalWrite(8, LOW);   //Disengage the Brake for Channel B
   analogWrite(11, 123);    //Spins the motor on Channel B at half speed
-
-  
-  delay(3000);
-
-  
-  digitalWrite(9, HIGH);  //Engage the Brake for Channel A
-  digitalWrite(8, HIGH);  //Engage the Brake for Channel B
-  
-  delay(1000);
   
   if (sensorState1 == HIGH || sensorState2 == HIGH) {
   // Si les "moustaches" ont une collision => les moteurs font reculer le robot pendant 3 sec 
